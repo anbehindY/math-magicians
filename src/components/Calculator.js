9db288eb051dc9e07672a126910717e359a3d842
anbehindY <yanminthwin@gmail.com>
2023-04-27 20:21:51 +0630

    Add keys to calculator JSX

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,25 +12,25 @@ function Calculator() {
           <button type="button" className="btn orange">&divide;</button>
         </div>
         <div className="row">
-          <button type="button" className="btn">7</button>
-          <button type="button" className="btn">8</button>
-          <button type="button" className="btn">9</button>
+          <button type="button" key={7} className="btn">7</button>
+          <button type="button" key={8} className="btn">8</button>
+          <button type="button" key={9} className="btn">9</button>
           <button type="button" className="btn orange">x</button>
         </div>
         <div className="row">
-          <button type="button" className="btn">4</button>
-          <button type="button" className="btn">5</button>
-          <button type="button" className="btn">6</button>
+          <button type="button" key={4} className="btn">4</button>
+          <button type="button" key={5} className="btn">5</button>
+          <button type="button" key={6} className="btn">6</button>
           <button type="button" className="btn orange">-</button>
         </div>
         <div className="row">
-          <button type="button" className="btn">1</button>
-          <button type="button" className="btn">2</button>
-          <button type="button" className="btn">3</button>
+          <button type="button" key={1} className="btn">1</button>
+          <button type="button" key={2} className="btn">2</button>
+          <button type="button" key={3} className="btn">3</button>
           <button type="button" className="btn orange">+</button>
         </div>
         <div className="lastRow">
-          <button type="button" className="btn zero">0</button>
+          <button type="button" key={0} className="btn zero">0</button>
           <button type="button" className="btn">.</button>
           <button type="button" className="btn orange">=</button>
         </div>
