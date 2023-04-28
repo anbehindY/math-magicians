b7bb68528b33b0569edef7aeffe3142ee6634335
anbehindY <yanminthwin@gmail.com>
2023-04-29 01:49:57 +0630

    Add quote jsx to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Calculator from './components/Calculator';
+import DisplayQuote from './components/displayQuote';
 import './index.css';
 
 function App() {
   return (
     <div className="App">
       <h1 className="header">Math Magicians</h1>
+      <DisplayQuote />
       <Calculator />
     </div>
   );
