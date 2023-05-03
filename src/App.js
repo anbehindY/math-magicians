7daa51a5f295e84e4d335bc13d021fcd820af137
anbehindY <yanminthwin@gmail.com>
2023-05-03 18:04:57 +0630

    Use routers and routes to create navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Calculator from './components/Calculator';
 import DisplayQuote from './components/displayQuote';
+import Navigation from './components/Navigation';
+import Home from './Home';
 import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="header">Math Magicians</h1>
-      <DisplayQuote />
-      <Calculator />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="calculator" element={<Calculator />} />
+          <Route path="quote" element={<DisplayQuote />} />
+        </Route>
+        <Route path="*" element={<div>Error! Page not found</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
